5321e144c0a5cb2f926834c8a7a9fa09cd7a23c7
Andres Barros <abarros17@cuc.edu.co>
2022-04-29 19:37:28 -0500

    Nuevo firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import 'firebase/firestore'
 
 const firebaseConfig = {
- apiKey: "AIzaSyAmHg0PH4s4V8zc_U8NXmquVf-WD3hjbns",
-  authDomain: "crud-5e6ed.firebaseapp.com",
-  projectId: "crud-5e6ed",
-  storageBucket: "crud-5e6ed.appspot.com",
-  messagingSenderId: "1052330145795",
-  appId: "1:1052330145795:web:78d5ccda88be3105e5b636"
+  apiKey: "AIzaSyCoHPZutlekD9I6gtmbCfLtYJt8DF_YaeQ",
+  authDomain: "crud-19e61.firebaseapp.com",
+  projectId: "crud-19e61",
+  storageBucket: "crud-19e61.appspot.com",
+  messagingSenderId: "630295244941",
+  appId: "1:630295244941:web:5370842c2f8586fba272c4"
 };
 
 // Initialize Firebase
